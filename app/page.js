e14fcf8672a603ad269b1935c3cef5850573cebe
darwin1501 <darwin.pablo1501@gmail.com>
2024-05-20 10:57:31 +0800

    add logic to import excel file

--- app/page.js
+++ app/page.js
@@ -1,13 +1,49 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+import * as XLSX from "xlsx";
 
 export default function Home() {
+
+  async function convertToJson(headers, data){
+    const rows = [];
+
+    data.forEach(async row => {
+      let rowData = {};
+
+      row.forEach(async (element, index) => {
+        rowData[headers[index]] = element;
+      })
+      rows.push(rowData)
+    });
+    console.log(rows);
+    return rows;
+  }
+
+  function importExcel(e){
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = async (event)=>{
+      const bstr = event.target.result;
+      const workBook = XLSX.read(bstr, {type: "binary"});
+      const workSheetName = workBook.SheetNames[0];
+      const workSheet = workBook.Sheets[workSheetName];
+      const fileData = XLSX.utils.sheet_to_json(workSheet, {header: 1});
+      const headers =   fileData[0];
+      const heads = headers.map(head => ({title: head, field: head}))
+      fileData.splice(0, 1);
+      convertToJson(headers, fileData);
+    }
+    reader.readAsArrayBuffer(file);
+  }
+
   return (
     <main className={styles.main}>
       <div>
           <div>
             <h1>Upload Excel File</h1>
-            <input type="file"/>
+            <input type="file" onChange={importExcel}/>
           </div>
           <br></br>
           <div>
